3a8d16c11d2166a6f7fdfcae149a6027826b0505
PAVILON GAMING <damarsaputra2266@gmail.com>
2021-11-25 17:32:52 +0700

    Create Project Sayuran

---
+++ src/main/java/ti/project/sayuran/sayur/sayuran/Application.java
@@ -0,0 +1,13 @@
+package ti.project.sayuran.sayur.sayuran;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ src/test/java/ti/project/sayuran/sayur/sayuran/ApplicationTests.java
@@ -0,0 +1,13 @@
+package ti.project.sayuran.sayur.sayuran;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
